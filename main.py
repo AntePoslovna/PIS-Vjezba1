bc4752e0d2da2ffa6c41ffb0ef39079b7d116d29
AntePoslovna <spajica1@gmail.com>
2025-03-19 09:57:58 +0100

    fix: riješen konflikt

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ def main():
 def nova_funkcija():
     return "Ovo je nova funkcija."
 # Ovo je moj komentar - Mirko Radan
+
+def novametoda(self):
+    return "Ovo je nova metoda"
+
 def nova_metoda():
     return "Ovo je nova metoda 2."
+
 if __name__ == "__main__":
     main()
 
-def nova_metoda(self):
-    return "Ovo je nova metoda"
