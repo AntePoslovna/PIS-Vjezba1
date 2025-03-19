c78a73f84563170c6788aa1d98900a2edc5485dd
M-Radan <147344008+M-Radan@users.noreply.github.com>
2025-03-19 09:50:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def main():
 def nova_funkcija():
     return "Ovo je nova funkcija."
 # Ovo je moj komentar - Mirko Radan
+def nova_metoda():
+    return "Ovo je nova metoda 2."
 if __name__ == "__main__":
     main()
